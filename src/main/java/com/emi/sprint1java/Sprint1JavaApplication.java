1b448812675c38274240bc25a4fb0e87c7962cc5
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-04-08 15:03:26 -0300

    First Commit

---
+++ src/main/java/com/emi/sprint1java/Sprint1JavaApplication.java
@@ -0,0 +1,13 @@
+package com.emi.sprint1java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint1JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprint1JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emi/sprint1java/Sprint1JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emi.sprint1java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint1JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
