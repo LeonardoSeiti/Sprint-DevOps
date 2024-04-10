ee7a38c363400891e789f2b11b60cc8d06c90793
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-04-10 19:42:27 -0300

    Classe Service criada.

---
+++ src/main/java/com/emi/sprint1java/service/ClienteService.java
@@ -0,0 +1,18 @@
+package com.emi.sprint1java.service;
+
+import com.emi.sprint1java.model.Cliente;
+
+import com.emi.sprint1java.dao.ClienteDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService{
+
+    @Autowired
+    private ClienteDAO clienteDAO;
+
+    public void salvar(Cliente cliente) {
+        ClienteDAO.salvar(cliente);
+    }
+}
