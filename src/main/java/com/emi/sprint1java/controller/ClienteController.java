9fd945978eff738678e3acb1d396298a936c3c43
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-04-13 17:26:03 -0300

    Método GET criado.

--- src/main/java/com/emi/sprint1java/controller/ClienteController.java
+++ src/main/java/com/emi/sprint1java/controller/ClienteController.java
@@ -4,9 +4,6 @@ import com.emi.sprint1java.model.Cliente;
 import com.emi.sprint1java.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
-import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("clientes")
--- src/main/java/com/emi/sprint1java/service/ClienteService.java
+++ src/main/java/com/emi/sprint1java/service/ClienteService.java
@@ -3,9 +3,7 @@ package com.emi.sprint1java.service;
 import com.emi.sprint1java.model.Cliente;
 import com.emi.sprint1java.dao.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ClienteService {
