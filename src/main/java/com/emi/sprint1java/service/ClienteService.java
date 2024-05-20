c8174e622b6a2d61924a41a4f4bba163eaa29c5a
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-05-19 22:43:30 -0300

    Correções da classe ClienteService.

--- src/main/java/com/emi/sprint1java/service/ClienteService.java
+++ src/main/java/com/emi/sprint1java/service/ClienteService.java
@@ -1,7 +1,7 @@
 package com.emi.sprint1java.service;
 
 import com.emi.sprint1java.model.Cliente;
-import com.emi.sprint1java.dao.ClienteRepository;
+import com.emi.sprint1java.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
